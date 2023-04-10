7755e996bfb20c0d5895522b82788796cf3356c9
longlc3 <94947386+longlc3@users.noreply.github.com>
2023-04-10 14:48:37 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,32 @@
 #!/bin/bash
-echo this is username: ${USER}
-echo this is password: ${PASSWORD}
+echo"
+imports:
+    - { resource: 'parameters.yml' }
+
+parameters:
+  database:
+    doctrine:
+      dbname: 'highlearning'
+      host: 'rds.prod.aurlom.local'
+      port: ~
+      user: 'administrator'
+      password: '${DB_PASSWORD}'
+  broker:
+    rabbitmq:
+      connections:
+        default:
+          host: '127.0.0.1'
+          port: 5672
+          login: 'highlearning'
+          password: '${RQ_PASSWORD}'
+          vhost: 'rmq.high-learning.com'
+  search:
+    elastic:
+      hosts:
+        - default:
+          host: 'elasticsearch.prod.aurlom.local'
+          port: '9200'
+          scheme: 'https'
+          user: 'highlearning'
+          pass: '${ES_PASSWORD}'
+" > parameters_prod.yml
